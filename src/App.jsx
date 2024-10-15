8cbad720308a19f9b42e9a80499ae01165923f30
divyanshucodes20 <155738552+divyanshucodes20@users.noreply.github.com>
2024-10-15 22:42:00 +0530

    Refactor tailwind and postcss configurations

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,19 @@ import Profile from "./components/Profile"
 function App() {
 
   return (
-    <UserContextProvider>
-      <Login/>
-      <Profile/>
-    </UserContextProvider>
+    
+<div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       
+                    </div>
+                </div>
+            </div>
+
   )
 }
 
